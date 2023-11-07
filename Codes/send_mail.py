bed963665f6571f7b95913e4e89f5ba65f789756
Nilesh Agarwalla <73545828+Nils13001@users.noreply.github.com>
2023-11-07 10:58:01 +0530

    Add-ons
    Added comments

--- Codes/send_mail.py
+++ Codes/send_mail.py
@@ -11,22 +11,26 @@ def mail(length, lengthp, img):
     with open(img, 'rb') as f:
         img_data = f.read()
 
+    # Parsing the config file created to understand the parameters.
     configur = ConfigParser()
     configur.read('configurations.ini')
 
     msg = MIMEMultipart()
 
+    # Providing Headers
     msg['Subject'] = 'Test mail with attachment'
     msg['From'] = configur.get('SMTPlogin', 'sender_address')
     msg['To'] = configur.get('SMTPlogin', 'receiver_address')
 
-    text = "Length of Key: {}; Length of Private Key: {}".format(str(length), str(lengthp))
-
+    # Providing Content
+    text = "Length of Symmetric Key: {}\nLength of Private Key: {}".format(str(length), str(lengthp))
     msg.attach(MIMEText(text, "plain"))
 
+    # Providing Image
     image = MIMEImage(img_data, name=basename(img))
     msg.attach(image)
 
+    # Providing the credentials and sending Email
     with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
         server.login(configur.get('SMTPlogin', 'mailtrap_user'), configur.get('SMTPlogin', 'mailtrap_password'))
         server.sendmail(configur.get('SMTPlogin', 'sender_address'), configur.get('SMTPlogin', 'receiver_address'), msg.as_string())
