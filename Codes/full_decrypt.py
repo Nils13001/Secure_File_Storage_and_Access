83a4af635da6cb0c1b106546aeeccc5ba5a5bfff
Nilesh Agarwalla <73545828+Nils13001@users.noreply.github.com>
2023-11-07 11:01:57 +0530

    Add-ons
    1. Commenting
    2. Mentioning about JPEG images
    3. Taking extension also as input for file

--- Codes/full_decrypt.py
+++ Codes/full_decrypt.py
@@ -1,15 +1,18 @@
 from PIL import Image
-
 import rsa
 from cryptography.fernet import Fernet
 from base64 import b64decode
 
 def img_decode(len1, lp):
     
-    img = input("Enter image name(with extension) : ")
+    # Taking the Stegged Image name and readying it
+    # Remember, no JPEG/JPG image
+    img = input("Enter the name of image downloaded from email (with extension) : ")
     image = Image.open(img, 'r')
-    data = ''
     imgdata = iter(image.getdata())
+    
+    # Extracting Symmetric Key
+    data = ''
     ct = 0
     ls = []
     while (True):
@@ -17,7 +20,8 @@ def img_decode(len1, lp):
 								imgdata.__next__()[:3] +
 								imgdata.__next__()[:3]]
         ct+=1
-        #Strings of binary data
+        
+        # Strings of binary data
         binstr = ''
         for i in pixels[:8]:
             if (i % 2 == 0):
@@ -30,6 +34,7 @@ def img_decode(len1, lp):
             ls.append(data)
             break
     
+    # Extracting Private Key
     priv = ''
     ct = 0
     while (True):
@@ -37,7 +42,7 @@ def img_decode(len1, lp):
 								imgdata.__next__()[:3] +
 								imgdata.__next__()[:3]]
         ct+=1
-        #Strings of binary data
+        # Strings of binary data
         binstr = ''
         for i in pixels[:8]:
             if (i % 2 == 0):
@@ -50,34 +55,39 @@ def img_decode(len1, lp):
             ls.append(priv)
             break
     
+    # First is Symmetric key and 2nd is Private Key
     return ls[0], ls[1]
 
 def dec_main():
+    # Provide the lengths of Symmetric key and Private key received on mail
     leng = int(input("Enter the length of key received on mail: "))
     lenp = int(input("Enter the length of private key received on mail: "))
-    k, pkp = img_decode(leng, lenp)
-    privkey = rsa.PrivateKey.load_pkcs1(pkp.encode('utf8'))
-    # Here we send payload over the network
-    # And receive it somewhere at the client side
 
-    # Decode the symmetric key from base64
+    # Provide the length parameters to retrieve the encrypted symmetric key and private key
+    k, pkp = img_decode(leng, lenp)
+
+    # Convert private key from string back to key object
+    privkey = rsa.PrivateKey.load_pkcs1(pkp.encode('utf8'))
+    
+    # Decode the encrypted symmetric key from base64
     enc_symmetricKey = b64decode(k)
 
-    enc_file = input("Enter name of encoded file: ")
+    # Provide the name of the encoded/encrypted file and read its content
+    enc_file = input("Enter the name of encoded file (with extension): ")
     with open (enc_file, 'rb') as file:
                 d = file.read()
 
-    # Decode the data from base64
+    # Decode the read data from base64
     enc_data = b64decode(d)
 
-    # Decrypt the symmetric key
+    # Decrypt the symmetric key with private key
     symmetricKey = rsa.decrypt(enc_symmetricKey, privkey)
 
     # Decrypt the data
     f = Fernet(symmetricKey)
     data = f.decrypt(enc_data)
 
-    dec_file = input("Enter name for decoded file: ")
+    # Provide a new file name and write the decrypted content to it
+    dec_file = input("Provide new name for decoded file (with extension): ")
     with open(dec_file, 'wb') as file:
         file.write(data)
-    print("Decryption Successful!!!")
