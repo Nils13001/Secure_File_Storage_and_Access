c6d70765a34ed4ed2d99877cf2f45a040334b2c6
Nilesh Agarwalla <73545828+Nils13001@users.noreply.github.com>
2023-11-07 11:09:47 +0530

    Added Feature
    Now, we can provide the sender and receiver email address as input

--- Codes/generate_mail_config.py
+++ Codes/generate_mail_config.py
@@ -6,10 +6,12 @@ config_file.add_section("SMTPlogin")
 
 # Add settings to section
 # Here you need to modify the values of last parameter of set function
-config_file.set("SMTPlogin", "sender_address", "<sender-email>")
-config_file.set("SMTPlogin", "receiver_address", "<receiver-email>")
-config_file.set("SMTPlogin", "mailtrap_user", "<SMTP user-id>")
-config_file.set("SMTPlogin", "mailtrap_password", "<SMTP password>")
+sender = input("Enter email address of sender: ")
+receiver = input("Enter email address of receiver: ")
+config_file.set("SMTPlogin", "sender_address", sender)
+config_file.set("SMTPlogin", "receiver_address", receiver)
+config_file.set("SMTPlogin", "mailtrap_user", "8893a2ba101d5f")
+config_file.set("SMTPlogin", "mailtrap_password", "1d160f6226faa1")
 
 # Saving config file as configurations.ini
 with open(r"configurations.ini", 'w') as configfileObj:
